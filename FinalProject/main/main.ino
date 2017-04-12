981ec82111184bc7fff6c3388d80485585a54e86
Brian McCarthy <brianmcca1@gmail.com>
2017-04-12 11:45:52 -0400

    Added main.ino file

---
+++ FinalProject/main/main.ino
@@ -0,0 +1,13 @@
+// RBE1001 Final Project
+// Team K
+// Brian McCarthy, Jonathon Brownlon, Zachary Zlotnick
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
