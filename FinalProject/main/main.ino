48c13a1e09441be2574ee8db88e64f1b4c8925e2
Brian McCarthy <brianmcca1@gmail.com>
2017-04-19 12:18:25 -0400

    Setup for Pneumatics, DFW controller, Motors

--- FinalProject/main/main.ino
+++ FinalProject/main/main.ino
@@ -3,17 +3,30 @@
 // Brian McCarthy, Jonathon Brownlon, Zachary Zlotnick
 
 #include <DFW.h>
+#include <Servo.h>
+
 enum state{
   AUTONOMOUS,
   TELEOP
 };
 
 state projectState;
+// TODO: Update to reflect actual Solenoid Pin
+int solenoidPinLeft = 4; // Solenoid for the left pneumatic 
+int solenoidPinRight = 5; // Solenoid for the right pneumatic
+int solenoidPinPEN = 6; // Solenoid for the pneumatic grabbing the PEN
+Servo rightMotor;
+Servo leftMotor;
 
 void setup() {
   // put your setup code here, to run once:
   projectState = AUTONOMOUS;
-  
+  pinMode(solenoidPinLeft, OUTPUT);
+  pinMode(solenoidPinRight, OUTPUT);
+  pinMode(solenoidPinPEN, OUTPUT);
+  dfw.begin();
+  leftMotor.attach(7, 1000, 2000);
+  rightMotor.attach(8, 1000, 2000);
 }
 
 void loop() {
@@ -24,6 +37,7 @@ void loop() {
       break;
     case TELEOP:
       // TELEOP CODE
+      dfw.run();
       break;
   }
 }
