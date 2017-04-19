485ccb54cbdcfc81bf64be28cc696685024822bf
Brian McCarthy <brianmcca1@gmail.com>
2017-04-19 10:18:29 -0400

    Added skeletons for state control

--- FinalProject/main/main.ino
+++ FinalProject/main/main.ino
@@ -2,12 +2,28 @@
 // Team K
 // Brian McCarthy, Jonathon Brownlon, Zachary Zlotnick
 
+#include <DFW.h>
+enum state{
+  AUTONOMOUS,
+  TELEOP
+};
+
+state projectState;
+
 void setup() {
   // put your setup code here, to run once:
-
+  projectState = AUTONOMOUS;
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  switch(projectState){
+    case AUTONOMOUS:
+      // AUTONOMOUS CODE
+      break;
+    case TELEOP:
+      // TELEOP CODE
+      break;
+  }
 }
